d92f2603126e7b02e1e2b9c5936d490401fcf0a4
yooniak <77361240+yooniak@users.noreply.github.com>
2024-01-27 21:32:09 +0100

    Some changes and fixes

--- main.go
+++ main.go
@@ -1,18 +1,41 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/betteerr/rssagg/internal/database"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("Port isnt found in enviroment")
+	}
+
+	dbUrl := os.Getenv("DB_URL")
+	if dbUrl == "" {
+		log.Fatal("Database URL isnt found in enviroment")
+	}
+
+	conn, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatal("Cannot connect to the database: ", err)
+	}
+
+	queries := database.New(conn)
+
+	apiCfg := apiConfig{
+		DB: queries,
 	}
 
 	router := chi.NewRouter()
@@ -30,6 +53,7 @@ func main() {
 
 	v1Router.Get("/healthz", handlerRediness)
 	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
@@ -39,7 +63,7 @@ func main() {
 	}
 
 	log.Printf("Server starting on port %v", portString)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
