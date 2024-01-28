5eb1bf996af4969104f91bdaae5d9b3267103a49
yooniak <77361240+yooniak@users.noreply.github.com>
2024-01-28 14:38:21 +0100

    Update in query and delete feed follow functionality

--- handler_feed_follows.go
+++ handler_feed_follows.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/betteerr/rssagg/internal/database"
+	"github.com/go-chi/chi"
 	"github.com/google/uuid"
 )
 
@@ -46,4 +47,23 @@ func (apiCfg *apiConfig) handlerGetFeedFollows(w http.ResponseWriter, r *http.Re
 	}
 
 	respondWithJSON(w, 201, databaseFeedFollowsToFeedFollows(feedFollows))
+}
+
+func (apiCfg *apiConfig) handlerDeleteFeedFollow(w http.ResponseWriter, r *http.Request, user database.User) {
+	feedFollowIDStr := chi.URLParam(r, "feedFollowID")
+	FeedFollowID, err := uuid.Parse(feedFollowIDStr)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldnt parse feed follow id: %v", err))
+		return
+	}
+
+	err = apiCfg.DB.DeleteFeedFollow(r.Context(), database.DeleteFeedFollowParams{
+		ID:     FeedFollowID,
+		UserID: user.ID,
+	})
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldnt delete feed follow: %v", err))
+		return
+	}
+	respondWithJSON(w, 200, struct{}{})
 }
